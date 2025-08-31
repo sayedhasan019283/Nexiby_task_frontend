8ab9a968993fa2dca459f2426697e463070ad6db
sayedhasan019283 <sayedhasan019283@gmail.com>
2025-09-01 01:22:54 +0600

    add full project task done

--- src/components/component/Navbar.tsx
+++ src/components/component/Navbar.tsx
@@ -4,7 +4,9 @@ const Navbar = () => {
   return (
     <nav className="bg-gray-800 p-4 shadow-md mt-11">
       <div className="flex justify-between items-center max-w-7xl mx-auto">
-        <div className="text-white text-xl font-bold">Task Manager</div>
+        <Link to="/" className="text-white text-xl font-bold">
+          Task Manager
+        </Link>
         <div className="space-x-4">
           <Link
             to="/"
